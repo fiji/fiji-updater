imagej/imagej-updater

Add MVS mediation engine for Maven-native update sites
Implements doc/maven-native-update-sites.md sections 3-4: the
net.imagej.updater.maven package with the component/managed index model
(GA, Coordinate, DependencyEdge, Release, MavenComponent, ManagedEntry,
ComponentCatalog), a DOM reader for the new index elements, Maven version
ordering (ported from jgo, the normative reference), and the MVSResolver
fixpoint with per-path exclusions, pin > BOM > MVS precedence, and
held-below / major-misalignment / missing-release warnings.

Validated against the golden mediation vectors shared with the Python
reference implementation (dbxm.mvs in the db-xml-maven repository); all
vectors must pass with roots in declared and reversed order.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

src/main/java/net/imagej/updater/maven/Coordinate.java
@@ -1,0 +1,78 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.updater.maven;
+
+/** A {@link GA} identity at a concrete version: {@code g:a:v}. */
+public final class Coordinate {
+
+	private final GA ga;
+	private final String version;
+
+	public Coordinate(final GA ga, final String version) {
+		this.ga = ga;
+		this.version = version;
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+
+	/** Parses {@code "g:a:v"} or {@code "g:a:c:p:v"}. */
+	public static Coordinate parse(final String s) {
+		final String[] tokens = s.split(":", -1);
+		if (tokens.length == 3) {
+			return new Coordinate(new GA(tokens[0], tokens[1]), tokens[2]);
+		}
+		if (tokens.length == 5) {
+			return new Coordinate(
+				new GA(tokens[0], tokens[1], tokens[2], tokens[3]), tokens[4]);
+		}
+		throw new IllegalArgumentException(
+			"Invalid coordinate (expected g:a:v or g:a:c:p:v): " + s);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (!(o instanceof Coordinate)) return false;
+		final Coordinate other = (Coordinate) o;
+		return ga.equals(other.ga) && version.equals(other.version);
+	}
+
+	@Override
+	public int hashCode() {
+		return ga.hashCode() * 31 + version.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

src/main/java/net/imagej/updater/maven/DependencyEdge.java
@@ -1,0 +1,73 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.updater.maven;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A direct dependency edge: target identity at a concrete version, with
+ * per-edge exclusions. Edges are publish-time facts (post model-building);
+ * version ranges are forbidden by the index format.
+ */
+public final class DependencyEdge {
+
+	private final GA ga;
+	private final String version;
+	private final List<Exclusion> exclusions;
+	private final String platform;
+	private final boolean optional;
+
+	public DependencyEdge(final GA ga, final String version,
+		final List<Exclusion> exclusions, final String platform,
+		final boolean optional)
+	{
+		this.ga = ga;
+		this.version = version;
+		this.exclusions = exclusions == null ? //
+			Collections.<Exclusion> emptyList() : //
+			Collections.unmodifiableList(exclusions);
+		this.platform = platform;
+		this.optional = optional;
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+	public List<Exclusion> exclusions() { return exclusions; }
+	public String platform() { return platform; }
+	public boolean optional() { return optional; }
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

src/main/java/net/imagej/updater/maven/Exclusion.java
@@ -1,0 +1,80 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.updater.maven;
+
+/**
+ * A {@code g:a} exclusion pattern; either field may be the wildcard
+ * {@code *}. Exclusions never carry versions, classifiers, or packaging
+ * (Maven rule).
+ */
+public final class Exclusion {
+
+	private final String group;
+	private final String artifact;
+
+	public Exclusion(final String group, final String artifact) {
+		this.group = group;
+		this.artifact = artifact;
+	}
+
+	/** Parses {@code "g:a"}; {@code *} allowed for either field. */
+	public static Exclusion parse(final String s) {
+		final String[] tokens = s.split(":", -1);
+		if (tokens.length != 2) {
+			throw new IllegalArgumentException(
+				"Invalid exclusion (expected g:a, * allowed): " + s);
+		}
+		return new Exclusion(tokens[0], tokens[1]);
+	}
+
+	public boolean matches(final GA ga) {
+		return ("*".equals(group) || group.equals(ga.group())) &&
+			("*".equals(artifact) || artifact.equals(ga.artifact()));
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (!(o instanceof Exclusion)) return false;
+		final Exclusion other = (Exclusion) o;
+		return group.equals(other.group) && artifact.equals(other.artifact);
+	}
+
+	@Override
+	public int hashCode() {
+		return group.hashCode() * 31 + artifact.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return group + ":" + artifact;
+	}
+}

src/main/java/net/imagej/updater/maven/ManagedEntry.java
@@ -1,0 +1,66 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.updater.maven;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * One {@code <managed><version>} entry: a BOM-pinned version, optionally
+ * with exclusions that apply to the managed component's outgoing edges
+ * wherever it appears (Maven depMgmt-exclusion semantics).
+ */
+public final class ManagedEntry {
+
+	private final GA ga;
+	private final String version;
+	private final List<Exclusion> exclusions;
+
+	public ManagedEntry(final GA ga, final String version,
+		final List<Exclusion> exclusions)
+	{
+		this.ga = ga;
+		this.version = version;
+		this.exclusions = exclusions == null ? //
+			Collections.<Exclusion> emptyList() : //
+			Collections.unmodifiableList(exclusions);
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+	public List<Exclusion> exclusions() { return exclusions; }
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

src/main/java/net/imagej/updater/maven/MavenComponent.java
@@ -1,0 +1,77 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.updater.maven;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * All known versions of one {@link GA}, plus site-level flags.
+ * <p>
+ * Presence in a catalog is a <em>fact</em>, not an installation
+ * instruction; only {@code offered} components can be enabled by users,
+ * and the installed set is always the output of mediation.
+ * </p>
+ */
+public final class MavenComponent {
+
+	private final GA ga;
+	private final Map<String, Release> releases = new LinkedHashMap<>();
+	private boolean offered;
+	private String current;
+
+	public MavenComponent(final GA ga) {
+		this.ga = ga;
+	}
+
+	public GA ga() { return ga; }
+	public Map<String, Release> releases() { return releases; }
+
+	public void add(final Release release) {
+		releases.put(release.version(), release);
+	}
+
+	public Release release(final String version) {
+		return releases.get(version);
+	}
+
+	public boolean offered() { return offered; }
+	public void setOffered(final boolean offered) { this.offered = offered; }
+
+	public String current() { return current; }
+	public void setCurrent(final String current) { this.current = current; }
+
+	@Override
+	public String toString() {
+		return ga.toString();
+	}
+}

fiji/fiji-updater

core/src/main/java/sc/fiji/updater/maven/Coordinate.java
@@ -1,0 +1,78 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package sc.fiji.updater.maven;
+
+/** A {@link GA} identity at a concrete version: {@code g:a:v}. */
+public final class Coordinate {
+
+	private final GA ga;
+	private final String version;
+
+	public Coordinate(final GA ga, final String version) {
+		this.ga = ga;
+		this.version = version;
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+
+	/** Parses {@code "g:a:v"} or {@code "g:a:c:p:v"}. */
+	public static Coordinate parse(final String s) {
+		final String[] tokens = s.split(":", -1);
+		if (tokens.length == 3) {
+			return new Coordinate(new GA(tokens[0], tokens[1]), tokens[2]);
+		}
+		if (tokens.length == 5) {
+			return new Coordinate(
+				new GA(tokens[0], tokens[1], tokens[2], tokens[3]), tokens[4]);
+		}
+		throw new IllegalArgumentException(
+			"Invalid coordinate (expected g:a:v or g:a:c:p:v): " + s);
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (!(o instanceof Coordinate)) return false;
+		final Coordinate other = (Coordinate) o;
+		return ga.equals(other.ga) && version.equals(other.version);
+	}
+
+	@Override
+	public int hashCode() {
+		return ga.hashCode() * 31 + version.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

core/src/main/java/sc/fiji/updater/maven/DependencyEdge.java
@@ -1,0 +1,73 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package sc.fiji.updater.maven;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A direct dependency edge: target identity at a concrete version, with
+ * per-edge exclusions. Edges are publish-time facts (post model-building);
+ * version ranges are forbidden by the index format.
+ */
+public final class DependencyEdge {
+
+	private final GA ga;
+	private final String version;
+	private final List<Exclusion> exclusions;
+	private final String platform;
+	private final boolean optional;
+
+	public DependencyEdge(final GA ga, final String version,
+		final List<Exclusion> exclusions, final String platform,
+		final boolean optional)
+	{
+		this.ga = ga;
+		this.version = version;
+		this.exclusions = exclusions == null ? //
+			Collections.<Exclusion> emptyList() : //
+			Collections.unmodifiableList(exclusions);
+		this.platform = platform;
+		this.optional = optional;
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+	public List<Exclusion> exclusions() { return exclusions; }
+	public String platform() { return platform; }
+	public boolean optional() { return optional; }
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

core/src/main/java/sc/fiji/updater/maven/Exclusion.java
@@ -1,0 +1,80 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package sc.fiji.updater.maven;
+
+/**
+ * A {@code g:a} exclusion pattern; either field may be the wildcard
+ * {@code *}. Exclusions never carry versions, classifiers, or packaging
+ * (Maven rule).
+ */
+public final class Exclusion {
+
+	private final String group;
+	private final String artifact;
+
+	public Exclusion(final String group, final String artifact) {
+		this.group = group;
+		this.artifact = artifact;
+	}
+
+	/** Parses {@code "g:a"}; {@code *} allowed for either field. */
+	public static Exclusion parse(final String s) {
+		final String[] tokens = s.split(":", -1);
+		if (tokens.length != 2) {
+			throw new IllegalArgumentException(
+				"Invalid exclusion (expected g:a, * allowed): " + s);
+		}
+		return new Exclusion(tokens[0], tokens[1]);
+	}
+
+	public boolean matches(final GA ga) {
+		return ("*".equals(group) || group.equals(ga.group())) &&
+			("*".equals(artifact) || artifact.equals(ga.artifact()));
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (!(o instanceof Exclusion)) return false;
+		final Exclusion other = (Exclusion) o;
+		return group.equals(other.group) && artifact.equals(other.artifact);
+	}
+
+	@Override
+	public int hashCode() {
+		return group.hashCode() * 31 + artifact.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return group + ":" + artifact;
+	}
+}

core/src/main/java/sc/fiji/updater/maven/ManagedEntry.java
@@ -1,0 +1,66 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package sc.fiji.updater.maven;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * One {@code <managed><version>} entry: a BOM-pinned version, optionally
+ * with exclusions that apply to the managed component's outgoing edges
+ * wherever it appears (Maven depMgmt-exclusion semantics).
+ */
+public final class ManagedEntry {
+
+	private final GA ga;
+	private final String version;
+	private final List<Exclusion> exclusions;
+
+	public ManagedEntry(final GA ga, final String version,
+		final List<Exclusion> exclusions)
+	{
+		this.ga = ga;
+		this.version = version;
+		this.exclusions = exclusions == null ? //
+			Collections.<Exclusion> emptyList() : //
+			Collections.unmodifiableList(exclusions);
+	}
+
+	public GA ga() { return ga; }
+	public String version() { return version; }
+	public List<Exclusion> exclusions() { return exclusions; }
+
+	@Override
+	public String toString() {
+		return ga + ":" + version;
+	}
+}

core/src/main/java/sc/fiji/updater/maven/MavenComponent.java
@@ -1,0 +1,77 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2025 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package sc.fiji.updater.maven;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * All known versions of one {@link GA}, plus site-level flags.
+ * <p>
+ * Presence in a catalog is a <em>fact</em>, not an installation
+ * instruction; only {@code offered} components can be enabled by users,
+ * and the installed set is always the output of mediation.
+ * </p>
+ */
+public final class MavenComponent {
+
+	private final GA ga;
+	private final Map<String, Release> releases = new LinkedHashMap<>();
+	private boolean offered;
+	private String current;
+
+	public MavenComponent(final GA ga) {
+		this.ga = ga;
+	}
+
+	public GA ga() { return ga; }
+	public Map<String, Release> releases() { return releases; }
+
+	public void add(final Release release) {
+		releases.put(release.version(), release);
+	}
+
+	public Release release(final String version) {
+		return releases.get(version);
+	}
+
+	public boolean offered() { return offered; }
+	public void setOffered(final boolean offered) { this.offered = offered; }
+
+	public String current() { return current; }
+	public void setCurrent(final String current) { this.current = current; }
+
+	@Override
+	public String toString() {
+		return ga.toString();
+	}
+}